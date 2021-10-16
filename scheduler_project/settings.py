13b5f96219b6c847b37b6348b7e212674d47cf53
mrudawski <m.rudawski777@gmail.com>
2021-10-17 00:49:45 +0200

    Update settings.py

--- scheduler_project/settings.py
+++ scheduler_project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'schedule.apps.ScheduleConfig',
+    'schedule',
     'django_celery_beat',
     'crispy_forms',
     'mathfilters'
